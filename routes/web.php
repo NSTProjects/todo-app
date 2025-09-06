95f15f4915a47265d56a63f4b82489dece1aa786
NSTProjects <nsttech.afg@gmail.com>
2025-09-06 14:28:50 +0430

    Post problem solve

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Livewire\Counter;
+use App\Livewire\Post;
 use App\Livewire\Todo;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::get('/', Todo::class)->name('home');
-Route::get('counter', Counter::class)->name('counter');
-Route::get('/post', Counter::class)->name('post');
+Route::get('/counter', Counter::class)->name('counter');
+Route::get('/post', Post::class)->name('post');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
